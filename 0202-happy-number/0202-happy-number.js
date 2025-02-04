05414450d3b0ca81b980e0700e506ce1f974701e
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-02-04 12:10:33 +0530

    Time: 1 ms (81.86%), Space: 51.1 MB (38.63%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let set = new Set()
+    while(true){
+        let sum = 0
+        while(n>0){
+            let dig = n%10
+            sum += dig*dig
+            n = Math.floor(n/10)
+
+        }
+
+        if(sum === 1 ) return true
+        if(set.has(sum)) break
+        else set.add(sum)
+
+        n = sum
+
+    }
+        return false
+};
