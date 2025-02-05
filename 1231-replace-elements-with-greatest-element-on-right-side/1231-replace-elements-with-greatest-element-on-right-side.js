aa7617f682cd0daf41f7a2c8ed821a132e7129cf
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-02-05 11:50:13 +0530

    Time: 1 ms (92.08%), Space: 59.2 MB (32.65%) - LeetHub

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+   let max = arr[arr.length-1]
+   arr[arr.length-1] = -1
+
+   for(let i = arr.length-2 ; i>=0 ; i--){
+     let temp = arr[i]
+     arr[i] = max
+     max = Math.max(temp,max)
+     
+   }
+
+  return arr
+};
