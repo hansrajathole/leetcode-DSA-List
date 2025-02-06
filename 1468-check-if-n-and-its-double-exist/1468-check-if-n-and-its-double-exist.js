a44f01ca5ebd029045b85b9375c17378a0e083c8
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-02-06 12:36:29 +0530

    Time: 2 ms (53.33%), Space: 50.8 MB (32.97%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.js
@@ -3,14 +3,23 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
+    // for(let i = 0 ; i<arr.length ; i++){
+    //     for(let j = 0 ; j<arr.length ; j++){
+    //         if(i!=j && arr[i]== (2*arr[j])){
+    //             return true
+    //         }
+    //     }
+    // }
+
+
+    let map = new Map()
     for(let i = 0 ; i<arr.length ; i++){
-        for(let j = 0 ; j<arr.length ; j++){
-            if(i!=j && arr[i]== (2*arr[j])){
-                return true
-            }
-        }
+        map.set(arr[i],i)
     }
 
+    for(let i = 0 ; i<arr.length ; i++){
+        if(map.has(arr[i]*2) && i!= map.get(arr[i]*2)) return true
+    }
 
     return false
 };
