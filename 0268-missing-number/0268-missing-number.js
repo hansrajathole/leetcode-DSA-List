f0aabd4f2fa0a31f87cf21eb1e2ad0dd98ae4fab
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-02-12 11:44:37 +0530

    Time: 19 ms (23.16%), Space: 53.1 MB (15.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -2,14 +2,46 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    let set = new Set(nums)
+var missingNumber = function(arr) {
 
-    for(let i = 0 ; i<= nums.length ; i++){
-        if(!set.has(i)){
-            return i
+
+    let n = arr.length
+    let i = 0
+    while( i < n) {
+        let correctIndex = arr[i]
+        if(arr[i]<arr.length){
+            if (arr[i] != arr[correctIndex]) {
+                let temp = arr[i]
+                arr[i] = arr[correctIndex]
+                arr[correctIndex] = temp
+                correctIndex = arr[i]
+            }else{
+                i++
+            }
+        }else{
+            i++ 
         }
     }
-    return 1
+    console.log(arr)
+    let j = 0
+    for(j ; j<arr.length ; j++){
+        if(arr[j] != j){
+            return j
+        }
+    }
+
+    return j
+
+
+
+
+    // let set = new Set(nums)
+
+    // for(let i = 0 ; i<= nums.length ; i++){
+    //     if(!set.has(i)){
+    //         return i
+    //     }
+    // }
+    // return 1
 
 };
