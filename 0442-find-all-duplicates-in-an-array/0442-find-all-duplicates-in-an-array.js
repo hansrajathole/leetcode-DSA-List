3564bb5287ecf7180d1a63e1406a4a329114d86c
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-02-13 00:51:57 +0530

    Time: 9 ms (68.24%), Space: 60.3 MB (58.5%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    let i = 0 
+    let n = nums.length
+    while(i<n){
+        let current = nums[i]-1
+        if(nums[i]!= nums[current]){
+            let temp = nums[i]
+            nums[i] = nums[current]
+            nums[current] = temp
+        }else{
+            i++
+        }
+    }
+
+
+    let arr = []
+    console.log(nums)
+    for(let i = 0 ; i<nums.length ; i++){
+        if(nums[i]!=i+1){
+            arr.push(nums[i])    
+        }
+    }  
+
+    return arr
+};
