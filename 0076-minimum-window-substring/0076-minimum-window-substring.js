242cfdca1d42dd3759c1b78543b04a666e3dff5c
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-04-24 13:01:55 +0530

    Time: 17 ms (90.46%), Space: 55.5 MB (83.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -7,28 +7,33 @@ var minWindow = function(s, t) {
     let map = new Map()
     let minLength = Infinity  , count = 0 , minStart = 0
 
-    for(let i = 0 ; i< t.length ; i++){
-        let ch = t.charAt(i)
-        map.set(ch , (map.get(ch) || 0 )+1)
-    }
-    let i = 0 , j = 0
-    while(j< s.length){
 
+    for(let i = 0 ; i<t.length ; i++){
+        let ch = t.charAt(i)
+        map.set(ch , (map.get(ch) || 0)+1)
+    }
+
+    let i = 0 , j = 0 
+
+    while(j<s.length){
         let ch = s.charAt(j)
         if(map.has(ch)){
-            map.set(ch , map.get(ch)-1)
-            if(map.get(ch)>=0) count++
+            map.set(ch ,map.get(ch)-1)
+            if(map.get(ch)>=0)count++
         }
 
         while(count === t.length){
-            if(minLength >j -i+1){
-                minLength = j - i +1
+            if(minLength > j-i+1){
+                minLength = j-i+1
                 minStart = i
             }
+
             let ch1 = s.charAt(i)
-             map.set(ch1 , map.get(ch1)+1)
-             if(map.get(ch1)>0)count--
-             i++
+            map.set(ch1 , map.get(ch1)+1)
+            if(map.get(ch1)>0)count--
+
+            i++
+
         }
 
         j++
