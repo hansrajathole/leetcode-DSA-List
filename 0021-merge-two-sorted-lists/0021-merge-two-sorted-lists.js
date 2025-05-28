41bcc85581ff11559d857b586269650ecdb62b69
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-05-28 19:11:50 +0530

    Time: 0 ms (100%), Space: 57.3 MB (65.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -6,22 +6,27 @@
  * }
  */
 /**
-
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
-    if(list1 == null) return list2
-    if(list2 == null) return list1
+    let dummy = new ListNode(-1);
+    let current = dummy;
 
-    if(list1.val < list2.val){
-        list1.next = mergeTwoLists(list1.next, list2)
-        return list1
-    }else{
-        list2.next = mergeTwoLists(list1, list2.next)
-        return list2
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
     }
 
+    // One of the lists might still have remaining nodes
+    current.next = list1 !== null ? list1 : list2;
+
+    return dummy.next;
 };
