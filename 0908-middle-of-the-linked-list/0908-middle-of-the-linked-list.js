e568b51ee9fcfdfda7266319096ee5706c463ea0
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2025-05-28 19:10:00 +0530

    Time: 0 ms (100%), Space: 54 MB (22.93%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
@@ -1,20 +1,15 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let slow = head , fast = head
-    while(fast != null && fast.next != null){
-        slow = slow.next
-        fast = fast.next.next
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
 
-    return slow
+    return slow;
 };
