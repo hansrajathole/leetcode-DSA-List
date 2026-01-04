c1afec591103814735ba2b9decb383d14e34419a
Hansraj Athole <140538035+hansrajathole@users.noreply.github.com>
2026-01-04 18:37:45 +0530

    Time: 6 ms (27.72%), Space: 46.3 MB (8.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+
+        String num = String.valueOf(x);
+        int i = 0 ;
+        int j = num.length()-1;
+
+        while(i<j){
+            if(num.charAt(i)!= num.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
